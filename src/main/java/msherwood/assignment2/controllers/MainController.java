37bebb9ab6968e63bcc9a6efdc8e02f5e5fcc002
Matthew Sherwood <matthew.sherwood@student.sl.on.ca>
2023-04-06 11:28:11 -0400

    added post and put mapping

--- src/main/java/msherwood/assignment2/controllers/MainController.java
+++ src/main/java/msherwood/assignment2/controllers/MainController.java
@@ -1,5 +1,6 @@
 package msherwood.assignment2.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -8,7 +9,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import msherwood.assignment2.model.Course;
@@ -49,6 +54,19 @@ public class MainController {
         return responseEntity;
     }
 
+    @PostMapping("/Courses/{orderId}")
+    public ResponseEntity<Integer> postCourse(@RequestBody Course newCourse, @PathVariable Integer Id ) {
+        Student student = resourceService.findStudentById(Id);
+
+        if(student != null) {
+            newCourse.setStudent(student);
+            resourceService.saveCourse(newCourse);
+            return new ResponseEntity<>(newCourse.getId(), HttpStatus.CREATED);
+        }
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
 
 // get mapping for all students 
     @GetMapping("/Students")
@@ -64,7 +82,7 @@ public class MainController {
         }
         return responseEntity;
     }
-
+// find student based off of the id 
     @GetMapping("/Students/{orderId}")
     public ResponseEntity<Student> getStudentById(@PathVariable Integer Id) {
         Student student = resourceService.findStudentById(Id);
@@ -72,5 +90,29 @@ public class MainController {
         responseEntity = new ResponseEntity<>(student, HttpStatus.OK);
         return responseEntity;
     }
+// Post mapping for student
+    @PutMapping("/Students/{orderId}")
+    public ResponseEntity<Integer> putStudent(@RequestBody Student newStudent, @PathVariable Integer Id) {
+        if(resourceService.updateStudent(newStudent, Id) == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+// get courses by using one student 
+    @GetMapping("/Students/{orderId}/Courses")
+    public ResponseEntity<List<Course>> getCoursesByStudent(@PathVariable Integer Id) {
+        Student existingStudent = resourceService.findStudentById(Id);
+        List<Course> studentCourses = new ArrayList<>();
+
+        studentCourses =resourceService.findCourseByStudent(existingStudent);
+        ResponseEntity<List<Course>> responseEntity;
+        responseEntity = new ResponseEntity<>(studentCourses, HttpStatus.OK);
+        return responseEntity;
+    }
+
+// 
+
     
 }
