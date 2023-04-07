24a2e87d081a40cf5666e14b477cc52f1d6a742c
Matthew Sherwood <matthew.sherwood@student.sl.on.ca>
2023-04-07 14:51:46 -0400

    course Repo documentation

--- src/main/java/msherwood/assignment2/model/CourseRepo.java
+++ src/main/java/msherwood/assignment2/model/CourseRepo.java
@@ -3,7 +3,7 @@ package msherwood.assignment2.model;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
-
+// allows the resourseService to use crudRepository queries 
 public interface CourseRepo  extends CrudRepository<Course, Integer>{
     List<Course> findByStudent(Student student);
     
--- src/main/java/msherwood/assignment2/model/StudentRepo.java
+++ src/main/java/msherwood/assignment2/model/StudentRepo.java
@@ -1,7 +1,7 @@
 package msherwood.assignment2.model;
 
 import org.springframework.data.repository.CrudRepository;
-
+// allows ResourseService to use crudRepository queries 
 public interface StudentRepo extends CrudRepository<Student, Integer> {
     
 }
