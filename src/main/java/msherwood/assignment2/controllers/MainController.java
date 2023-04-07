0539f8a33def4fe7ff8f3027c0d092892f36e5c4
Matthew Sherwood <matthew.sherwood@student.sl.on.ca>
2023-04-07 14:46:22 -0400

    added the documentation of MainController

--- src/main/java/msherwood/assignment2/controllers/MainController.java
+++ src/main/java/msherwood/assignment2/controllers/MainController.java
@@ -22,10 +22,11 @@ import msherwood.assignment2.services.ResourceService;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:9090")
+@CrossOrigin(origins = "http://localhost:9090") // sends the CORS the propper http requests without getting an error for cross site scripting
 public class MainController {
     ResourceService resourceService; // a local instance of our service file
 
+    // injecting the resourse service into the constructor for the main controller 
     public MainController(ResourceService resourceService) {
         this.resourceService = resourceService;
     }
@@ -33,9 +34,10 @@ public class MainController {
 // get mapping for all cources 
     @GetMapping("/Courses") 
     public ResponseEntity<List<Course>> getAllCources() {
+    // stores all of the courses into this list
         List<Course> courses = resourceService.findAllCourses();
         ResponseEntity<List<Course>> responseEntity;
-
+    // checks to see if the list is empty or full and sends the data and the propper request staus
         if(courses != null) {
             responseEntity = new ResponseEntity<>(courses, HttpStatus.OK);
         }
@@ -48,6 +50,7 @@ public class MainController {
 // find course with matching id
     @GetMapping("/Courses/{Id}")
     public ResponseEntity<Course> getCourseById(@PathVariable Integer Id) {
+    // using the Id passed from the params the course will find the student 
         Course course = resourceService.findCourseById(Id);
         ResponseEntity<Course> responseEntity;
         responseEntity = new ResponseEntity<>(course, HttpStatus.OK);
@@ -56,8 +59,9 @@ public class MainController {
 // post mapping for courses
     @PostMapping("/Courses/{Id}")
     public ResponseEntity<Integer> postCourse(@RequestBody Course newCourse, @PathVariable Integer Id ) {
+    // passes the id to find a spesific student
         Student student = resourceService.findStudentById(Id);
-
+    // if student is not empty it will add a course onto the student and pass a created http status
         if(student != null) {
             newCourse.setStudent(student);
             resourceService.saveCourse(newCourse);
@@ -69,6 +73,7 @@ public class MainController {
 // delete mapping for courses 
     @DeleteMapping("/Courses{Id}")
     public ResponseEntity<Integer> deleteCourse(@PathVariable Integer Id) {
+    // this will use the deleteCourse from the resourse service using the Id passed and send either a ok http request if it finds the id
         if(resourceService.deleteCourses(Id)) {
             return new ResponseEntity<>(HttpStatus.OK);
         }
@@ -79,9 +84,10 @@ public class MainController {
 // get mapping for all students 
     @GetMapping("/Students")
     public ResponseEntity<List<Student>> getAllStudents() {
+    // store all students into the list
         List<Student> students = resourceService.findAllStudents();
         ResponseEntity<List<Student>> responseEntity;
-
+    // if not empty it will return the data and a ok http request
         if(students != null) {
             responseEntity = new ResponseEntity<>(students, HttpStatus.OK);
         }
@@ -93,6 +99,7 @@ public class MainController {
 // find student based off of the id 
     @GetMapping("/Students/{Id}")
     public ResponseEntity<Student> getStudentById(@PathVariable Integer Id) {
+    // using the passed in data it will find the student using the id
         Student student = resourceService.findStudentById(Id);
         ResponseEntity<Student> responseEntity;
         responseEntity = new ResponseEntity<>(student, HttpStatus.OK);
@@ -101,6 +108,7 @@ public class MainController {
 // post mapping for student 
     @PostMapping("/Students")
     public ResponseEntity<Integer> postStudent(@RequestBody Student student) {
+    // with will create a new student using the resourseService query
         resourceService.saveStudent(student);
         return new ResponseEntity<>(student.getId(), HttpStatus.CREATED);
 
@@ -108,6 +116,7 @@ public class MainController {
 // put mapping for student
     @PutMapping("/Students/{Id}")
     public ResponseEntity<Integer> putStudent(@RequestBody Student newStudent, @PathVariable Integer Id) {
+    // this will update the student name
         if(resourceService.updateStudent(newStudent, Id) == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
@@ -118,6 +127,7 @@ public class MainController {
 // delete mapping for student
     @DeleteMapping("Students/{Id}")
     public ResponseEntity<Integer> deleteStudent(@PathVariable Integer Id) {
+    // using the passed in id it will find the student and use the resourse service to delete the student
         if(resourceService.deleteStudent(Id)) {
             return new ResponseEntity<>(HttpStatus.OK);
         }
@@ -127,11 +137,14 @@ public class MainController {
 // get courses by using one student 
     @GetMapping("/Students/{Id}/Courses")
     public ResponseEntity<List<Course>> getCoursesByStudent(@PathVariable Integer Id) {
+    // using the passed in param it will find the student
         Student existingStudent = resourceService.findStudentById(Id);
+    // this will store all courses under the student 
         List<Course> studentCourses = new ArrayList<>();
-
-        studentCourses =resourceService.findCourseByStudent(existingStudent);
+    // using a query from resourceService it will find the course by the student passed 
+        studentCourses = resourceService.findCourseByStudent(existingStudent);
         ResponseEntity<List<Course>> responseEntity;
+    // this will pass the student courses assocetated with the student and a http response of ok
         responseEntity = new ResponseEntity<>(studentCourses, HttpStatus.OK);
         return responseEntity;
     }
